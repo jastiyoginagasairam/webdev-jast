fd5169b2c3f25a721d9c304dd06bbe2ced3afe1c
jastiyoginagasairam <111644440+jastiyoginagasairam@users.noreply.github.com>
2022-08-27 21:43:31 +0530

    Add files via upload

---
+++ calcu.js
@@ -0,0 +1,10 @@
+const array=[];
+function display(n){
+    array.push(n);   
+}
+function results(){
+        document.write(eval(array.join("")));
+}
+function result(){
+    document.write("0");
+}
